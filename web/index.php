5ea43c685e0169a8f5133957b599950a1767634a
William Notowidagdo <wnotowidagdo@gmail.com>
2013-09-23 16:46:26 +0700

    Added - endpoints.json: configuration file to store list of endpoints and - GET /endpoints - route to get list of endpoints

--- web/index.php
+++ web/index.php
@@ -4,4 +4,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->get('/endpoints', function () use ($app) {
+
+    $endpoints = json_decode(file_get_contents(__DIR__.'/../endpoints.json'), true);
+    $endpoints = $endpoints['endpoints'];
+
+    $output = array(
+        'count' => count($endpoints),
+        'data' => $endpoints
+    );
+
+    return $app->json($output);
+});
+
 $app->run();
