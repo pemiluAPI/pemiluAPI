2bc3fc0f847e615deada5b66b52cab4193c2b071
William Notowidagdo <wnotowidagdo@gmail.com>
2013-10-07 16:59:31 +0700

    Added route that dispatch to endpoint URL

--- src/app.php
+++ src/app.php
@@ -16,6 +16,71 @@ $app->get('/', function () use ($app) {
     return $app->json($output, 404);
 });
 
+// Read configuration
+$config = json_decode(file_get_contents(__DIR__.'/../pemiluapi.json'), true);
+
+// Load available endpoints
+$endpoints = json_decode(file_get_contents(__DIR__.'/../endpoints.json'), true);
+
+// Define endpoint routes
+foreach ($endpoints['endpoints'] as $slug => $attributes):
+
+    $app->match('/'.$slug.'/api/{resource}', function (Request $request, $resource) use ($app, $attributes, $config) {
+
+        // Try authenticate apiKey
+        $client = new Client($config['host'], array(
+            'request.options' => array(
+                'query' => array('apiKey' => $request->get('apiKey')),
+                'exceptions' => false
+            )
+        ));
+
+        // Authenticate
+        $response = $client->get('/api/authenticate')->send();
+
+        // Failed? Immediately return 401
+        if ($response->getStatusCode() == 401) {
+            $output = array(
+                'error' => array(
+                    'type' => 'invalid_request_error'
+                )
+            );
+
+            return $app->json($output, 401);
+        }
+
+        // Set the API server base
+        $client = new Client($attributes['base'], array(
+            'request.options' => array(
+                'exceptions' => false
+            )
+        ));
+
+        // Call the endpoint
+        $response = $client->createRequest($request->getMethod(), '/api/' . $resource)->send();
+
+        // Prepare output
+        switch ($response->getStatusCode()) {
+            case 404:
+                $output = array(
+                    'error' => array(
+                        'type' => 'data_not_found'
+                    )
+                );
+                break;
+            default:
+                $output = array(
+                    'data' => $response->json()
+                );
+                break;
+         };
+
+        return $app->json($output, $response->getStatusCode());
+
+    })->method('GET|POST');// Only match GET or POST
+
+endforeach; // eo Define endpoint routes
+
 $app->get('/endpoints', function (Request $request) use ($app) {
     // Read configuration
     $config = json_decode(file_get_contents(__DIR__.'/../pemiluapi.json'), true);
