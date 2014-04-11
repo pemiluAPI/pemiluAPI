12121bce45a6256163f4fe2f2d45660b3f1b7c6f
Erie <erie.suharko@wgs.co.id>
2014-04-11 11:24:30 +0700

    update handler when not send Accept-Version header

--- src/app.php
+++ src/app.php
@@ -80,7 +80,7 @@ foreach ($endpoints['endpoints'] as $slug => $attributes):
         $client = $client->createRequest(
             $request->getMethod(), // method
             $attributes['base'] . '/api/' . $resource . '/' . $id, // uri
-            array('Content-Type' => 'application/x-www-form-urlencoded', 'Accept-Version' => (isset($head["Accept-Version"])) ? $head["Accept-Version"] : null), // headers
+            (isset($head["Accept-Version"])) ? array('Content-Type' => 'application/x-www-form-urlencoded', 'Accept-Version' => $head["Accept-Version"]) : array('Content-Type' => 'application/x-www-form-urlencoded'), // headers
             $request->getContent() // body
         );
 
